92f94a0cc0fd32d0623c63ba9f3ccc7f535cd5b8
Felipe Malacarne <105752743+FelipeMalacarne@users.noreply.github.com>
2023-06-08 03:04:47 -0300

    Email Service and Controller

--- src/main/java/com/example/msemail/controller/EmailController.java
+++ src/main/java/com/example/msemail/controller/EmailController.java
@@ -1,2 +1,32 @@
-package com.example.msemail.controller;public class EmailController {
+package com.example.msemail.controller;
+
+import com.example.msemail.dto.EmailDto;
+import com.example.msemail.model.Email;
+import com.example.msemail.service.EmailService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    EmailService emailService;
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<Email> sendingEmail(@RequestBody @Valid EmailDto emailDto){
+        Email email = new Email();
+        BeanUtils.copyProperties(emailDto, email);
+        emailService.sendEmail(email);
+        return new ResponseEntity<>(email, HttpStatus.CREATED);
+    }
+
+
+
 }
--- src/main/java/com/example/msemail/service/EmailService.java
+++ src/main/java/com/example/msemail/service/EmailService.java
@@ -1,2 +1,17 @@
-package com.example.msemail.service;public class EmailService {
+package com.example.msemail.service;
+
+import com.example.msemail.dao.EmailRepository;
+import com.example.msemail.model.Email;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    EmailRepository emailRepository;
+
+    public void sendEmail(Email email) {
+
+    }
 }
