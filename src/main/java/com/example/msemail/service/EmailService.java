7b35508026e857dd5a4c96a4280b4c098194143d
Felipe Malacarne <105752743+FelipeMalacarne@users.noreply.github.com>
2023-06-08 03:35:01 -0300

    sending email working

--- src/main/java/com/example/msemail/service/EmailService.java
+++ src/main/java/com/example/msemail/service/EmailService.java
@@ -1,9 +1,15 @@
 package com.example.msemail.service;
 
 import com.example.msemail.dao.EmailRepository;
+import com.example.msemail.enums.StatusEmail;
 import com.example.msemail.model.Email;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -11,7 +17,25 @@ public class EmailService {
     @Autowired
     EmailRepository emailRepository;
 
-    public void sendEmail(Email email) {
+    @Autowired
+    JavaMailSender mailSender;
 
+    public Email sendEmail(Email email) {
+        email.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(email.getEmailFrom());
+            message.setTo(email.getEmailTo());
+            message.setSubject(email.getSubject());
+            message.setText(email.getText());
+            mailSender.send(message);
+
+            email.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e){
+            e.printStackTrace();
+            email.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(email);
+        }
     }
 }
